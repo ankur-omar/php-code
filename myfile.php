1b3e9c33c94dde925a462f0c6247d7bc7f718058
xop-er <ankur.omar_cs17@gla.ac.in>
2020-01-13 14:29:44 +0530

    commit

---
+++ myfile.php
@@ -0,0 +1,27 @@
+<?php
+echo " this is my first php code","i am ankur omar"," your php student";// echo is use for print the multiple argument. ech does not return the value.
+echo '<br>';
+print "this is to print the output onto screen"// it's print only for single arguments and return the value;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1 style ="color:red";> 
+<?php
+echo " my name is ankur omar";
+?>
+</h1>
+
+    
+</body>
+</html>
+
+
+
